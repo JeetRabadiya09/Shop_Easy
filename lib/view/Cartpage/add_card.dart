858a8bf9c1d168664252dc7609aeea2dfaf0d012
Jeet Rabadiya <jeetrabadiya876@gmail.com>
2023-09-11 18:47:22 +0530

    changes

--- lib/view/Cartpage/add_card.dart
+++ lib/view/Cartpage/add_card.dart
@@ -33,6 +33,11 @@ class _AddCardScreenState extends State<AddCardScreen> {
           children: [
             TextField(
               decoration: InputDecoration(
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Image.asset("assets/images2/Mastercard.png",
+                      height: 38, width: 25),
+                ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(5),
                   borderSide:
@@ -40,14 +45,112 @@ class _AddCardScreenState extends State<AddCardScreen> {
                 ),
                 // contentPadding: const EdgeInsets.all(00),
                 isDense: true,
+
                 labelText: "Card Number",
+                labelStyle: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFFA1A1A1)),
                 // hintText: "Enter email ",
                 contentPadding: const EdgeInsets.all(12),
-                hintStyle: const TextStyle(
-                    color: Color(0xFFB3B3B3),
-                    fontSize: 16,
+                // hintStyle: const TextStyle(
+                //     color: Color(0xFFB3B3B3),
+                //     fontSize: 16,
+                //     fontWeight: FontWeight.w400,
+                //     fontFamily: "Poppins"),
+              ),
+              onTap: () {},
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide:
+                      const BorderSide(width: 0.8, color: Color(0x8EB3B3B3)),
+                ),
+                // contentPadding: const EdgeInsets.all(00),
+                isDense: false,
+                labelText: "Cardholder’s Name",
+                labelStyle: const TextStyle(
+                    fontSize: 14,
                     fontWeight: FontWeight.w400,
-                    fontFamily: "Poppins"),
+                    color: Color(0xFFA1A1A1)),
+                // hintText: "Enter email ",
+                contentPadding: const EdgeInsets.all(12),
+                // hintStyle: const TextStyle(
+                //     color: Color(0xFFB3B3B3),
+                //     fontSize: 16,
+                //     fontWeight: FontWeight.w400,
+                //     fontFamily: "Poppins"),
+              ),
+              onTap: () {},
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                suffixIcon: const Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(Icons.calendar_month)
+                    // Image.asset("assets/images2/Mastercard.png",
+                    //     height: 38, width: 25),
+                    ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide:
+                      const BorderSide(width: 0.8, color: Color(0x8EB3B3B3)),
+                ),
+                // contentPadding: const EdgeInsets.all(00),
+                isDense: false,
+                labelText: "Expiry Date",
+                labelStyle: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFFA1A1A1)),
+                // hintText: "Enter email ",
+                contentPadding: const EdgeInsets.all(12),
+                // hintStyle: const TextStyle(
+                //     color: Color(0xFFB3B3B3),
+                //     fontSize: 16,
+                //     fontWeight: FontWeight.w400,
+                //     fontFamily: "Poppins"),
+              ),
+              onTap: () {},
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                suffixIcon: const Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(Icons.info_outline)
+                    // Image.asset("assets/images2/Mastercard.png",
+                    //     height: 38, width: 25),
+                    ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide:
+                      const BorderSide(width: 0.8, color: Color(0x8EB3B3B3)),
+                ),
+                // contentPadding: const EdgeInsets.all(00),
+                isDense: false,
+                labelText: "CVC/CVV",
+                labelStyle: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFFA1A1A1)),
+                // hintText: "Enter email ",
+                contentPadding: const EdgeInsets.all(12),
+                // hintStyle: const TextStyle(
+                //     color: Color(0xFFB3B3B3),
+                //     fontSize: 16,
+                //     fontWeight: FontWeight.w400,
+                //     fontFamily: "Poppins"),
               ),
               onTap: () {},
             ),
--- lib/view/Log_in/confirm_email.dart
+++ lib/view/Log_in/confirm_email.dart
@@ -20,127 +20,131 @@ class _ConfirmEmailState extends State<ConfirmEmail> {
     double screenWidth = size.width;
     EdgeInsets devicePadding = MediaQuery.of(context).viewPadding;
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(screenWidth / 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              AppStrings.confirmemail,
-              style: TextStyle(
-                  color: AppColors.black,
-                  fontFamily: "Poppins",
-                  fontSize: 22,
-                  fontWeight: FontWeight.w500),
-            ),
-            SizedBox(
-              height: screenHeight / 20,
-            ),
-            Center(
-              child: Image.asset(
-                AppAssets.imageemail,
-                height: screenHeight / 3,
-                width: screenWidth / 1.5,
-              ),
-            ),
-            SizedBox(
-              height: screenHeight / 30,
-            ),
-            const Text(
-              AppStrings.confirmemail1,
-              style: TextStyle(
-                  color: AppColors.black,
-                  fontFamily: "Poppins",
-                  fontSize: 13,
-                  fontWeight: FontWeight.w400),
-            ),
-            const Text(
-              AppStrings.confirmemail2,
-              style: TextStyle(
-                  color: AppColors.black,
-                  fontFamily: "Poppins",
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600),
-            ),
-            const Text(
-              AppStrings.confirmemail3,
-              style: TextStyle(
-                  color: AppColors.black,
-                  fontFamily: "Poppins",
-                  fontSize: 13,
-                  fontWeight: FontWeight.w400),
-            ),
-            SizedBox(
-              height: screenHeight / 40,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                  shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6))),
-                  fixedSize: MaterialStatePropertyAll(
-                      Size(screenWidth / 1, screenHeight / 18)),
-                  backgroundColor:
-                      const MaterialStatePropertyAll(AppColors.orange)),
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MainHomeScreen(),
-                    ),
-                    (route) => false);
-              },
-              child: const Text(
-                "Continue",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontFamily: "Poppins",
-                  fontWeight: FontWeight.w500,
-                  color: AppColors.white,
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(screenWidth / 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  AppStrings.confirmemail,
+                  style: TextStyle(
+                      color: AppColors.black,
+                      fontFamily: "Poppins",
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500),
                 ),
-              ),
+                SizedBox(
+                  height: screenHeight / 20,
+                ),
+                Center(
+                  child: Image.asset(
+                    AppAssets.imageemail,
+                    height: screenHeight / 3,
+                    width: screenWidth / 1.5,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight / 30,
+                ),
+                const Text(
+                  AppStrings.confirmemail1,
+                  style: TextStyle(
+                      color: AppColors.black,
+                      fontFamily: "Poppins",
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400),
+                ),
+                const Text(
+                  AppStrings.confirmemail2,
+                  style: TextStyle(
+                      color: AppColors.black,
+                      fontFamily: "Poppins",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600),
+                ),
+                const Text(
+                  AppStrings.confirmemail3,
+                  style: TextStyle(
+                      color: AppColors.black,
+                      fontFamily: "Poppins",
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400),
+                ),
+                SizedBox(
+                  height: screenHeight / 40,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6))),
+                      fixedSize: MaterialStatePropertyAll(
+                          Size(screenWidth / 1, screenHeight / 18)),
+                      backgroundColor:
+                          const MaterialStatePropertyAll(AppColors.orange)),
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MainHomeScreen(),
+                        ),
+                        (route) => false);
+                  },
+                  child: const Text(
+                    "Continue",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.white,
+                    ),
+                  ),
+                ),
+                // GestureDetector(
+                //   onTap: () {
+                //     Navigator.pushAndRemoveUntil(
+                //         context,
+                //         MaterialPageRoute(
+                //           builder: (context) => MainHomeScreen(),
+                //         ),
+                //         (route) => false);
+                //   },
+                //   child: Container(
+                //     height: screenHeight / 17,
+                //     width: double.infinity,
+                //     alignment: Alignment.center,
+                //     decoration: BoxDecoration(
+                //       color: AppColors.orange,
+                //       borderRadius: BorderRadius.circular(10),
+                //     ),
+                //     child: const Text(
+                //       "Continue",
+                //       style: TextStyle(
+                //         fontSize: 14,
+                //         fontFamily: "Poppins",
+                //         fontWeight: FontWeight.w500,
+                //         color: AppColors.white,
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                SizedBox(
+                  height: screenHeight / 40,
+                ),
+                const Text(
+                  "Resend Email",
+                  style: TextStyle(
+                      color: AppColors.orangetwo,
+                      fontFamily: "Poppins",
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500),
+                ),
+              ],
             ),
-            // GestureDetector(
-            //   onTap: () {
-            //     Navigator.pushAndRemoveUntil(
-            //         context,
-            //         MaterialPageRoute(
-            //           builder: (context) => MainHomeScreen(),
-            //         ),
-            //         (route) => false);
-            //   },
-            //   child: Container(
-            //     height: screenHeight / 17,
-            //     width: double.infinity,
-            //     alignment: Alignment.center,
-            //     decoration: BoxDecoration(
-            //       color: AppColors.orange,
-            //       borderRadius: BorderRadius.circular(10),
-            //     ),
-            //     child: const Text(
-            //       "Continue",
-            //       style: TextStyle(
-            //         fontSize: 14,
-            //         fontFamily: "Poppins",
-            //         fontWeight: FontWeight.w500,
-            //         color: AppColors.white,
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            SizedBox(
-              height: screenHeight / 40,
-            ),
-            const Text(
-              "Resend Email",
-              style: TextStyle(
-                  color: AppColors.orangetwo,
-                  fontFamily: "Poppins",
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
